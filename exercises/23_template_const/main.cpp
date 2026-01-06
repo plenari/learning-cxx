abfc327f3ad11d2b26e0e18311604a73fd688eec
plenari <shengjiex@qq.com>
2026-01-06 17:24:42 +0800

    done

--- exercises/23_template_const/main.cpp
+++ exercises/23_template_const/main.cpp
@@ -1,4 +1,4 @@
-﻿#include "../exercise.h"
+#include "../exercise.h"
 #include <cstring>
 
 // READ: 模板非类型实参 <https://zh.cppreference.com/w/cpp/language/template_parameters#%E6%A8%A1%E6%9D%BF%E9%9D%9E%E7%B1%BB%E5%9E%8B%E5%AE%9E%E5%8F%82>
@@ -11,6 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (unsigned int i = 0; i < N; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -32,9 +36,13 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
+        unsigned int stride = 1;
         for (unsigned int i = 0; i < N; ++i) {
-            ASSERT(indices[i] < shape[i], "Invalid index");
+            unsigned int idx = N - 1 - i;
+            ASSERT(indices[idx] < shape[idx], "Invalid index");
             // TODO: 计算 index
+            index += indices[idx] * stride;
+            stride *= shape[idx];
         }
         return index;
     }
